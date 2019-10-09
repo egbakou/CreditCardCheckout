191a413c2d516c6b92ce4f33aca20d9acd6e3a59
egbakou <egbakoukodjolaurent@gmail.com>
2019-10-09 15:25:24 +0000

    DataTemplate for CardsView added.

---
+++ ShopTimeline/ShopTimeline/Views/Templates/CardTemplate.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShopTimeline.Views.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CardTemplate : ContentView
+    {
+        public CardTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
